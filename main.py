42ab058ad6efc67f3704941cf213bcef3796025f
defjoy <defjoy9@users.noreply.github.com>
2025-05-04 14:11:51 -0400

    added comments

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 from yt_dlp import YoutubeDL
 
 def main():
+    # user enters youtube URL
     yt_url = input("Enter youtube URL here: ")
     while "https://www.youtube.com/" not in yt_url:
         yt_url = input("Invalid URL, try again: ")
 
+    # user selects desired quality
     quality = input("Which mp3 quality do you want to download (128kbps,192kbps,320kbps): ")
     while quality not in ("128", "192", "320"):
         quality = input("Invalid quality, try again (128/192/320): ")
 
-
+    # setting up all the required options to download URL
     ydl_opts = {
         'format': 'bestaudio/best',
-        'outtmpl': '%(title)s_opus.%(ext)s',
+        'outtmpl': '%(title)s.%(ext)s',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
             'preferredquality': quality
 }]}
-
-    with YoutubeDL(ydl_opts) as ydl:
-        ydl.download([yt_url])
-
+    # converting audio to mp3 and saves the {codec} file.
+    try:
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download([yt_url])
+    except Exception as error:
+        print(f"WARNING! AN EXCEPTION OCURRED: {error}")
 
 if __name__ == "__main__":
     main()
